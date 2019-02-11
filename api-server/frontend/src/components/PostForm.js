9ac4be7775752abe5fdc2ee0b15960f4b19a7c1e
Vlad <vovaturcan4@gmail.com>
2019-02-12 00:42:01 +0200

    added some validation to the form

--- api-server/frontend/src/components/PostForm.js
+++ api-server/frontend/src/components/PostForm.js
@@ -1,23 +1,37 @@
 import React from "react";
 import { withFormik, Form, Field } from "formik";
-import Yup from "yup";
+import * as yup from "yup";
 import { submitPost } from "../actions";
 import moment from "moment";
 import uuid from "uuid";
 
-function PostForm({ values, erros, touched, isSubmitting, handleChange }) {
+console.log(yup);
+
+function PostForm({ values, errors, touched, isSubmitting, handleChange }) {
   return (
     <Form>
-      <Field type="text" name="title" placeholder="Your title here" />
-      <Field type="text" name="author" placeholder="Author" />
-      <textarea
-        onChange={handleChange}
-        value={values.body}
-        name="body"
-        cols="30"
-        rows="10"
-        placeholder="Your article goes here"
-      />
+      <div>
+        {touched.title && errors.title && <p>{errors.title}</p>}
+        <Field type="text" name="title" placeholder="Your title here" />
+      </div>
+
+      <div>
+        {touched.author && errors.author && <p>{errors.author}</p>}
+        <Field type="text" name="author" placeholder="Author" />
+      </div>
+
+      <div>
+        {touched.body && errors.body && <p>{errors.body}</p>}
+        <textarea
+          onChange={handleChange}
+          value={values.body}
+          name="body"
+          cols="30"
+          rows="10"
+          placeholder="Your article goes here"
+        />
+      </div>
+
       <Field component="select" name="category">
         <option value="react">React</option>
         <option value="redux">Redux</option>
@@ -39,6 +53,13 @@ export default withFormik({
       category: category || "react"
     };
   },
+  validationSchema: yup.object().shape({
+    title: yup.string().required("You must enter a title"),
+    author: yup.string().required("Name the writer"),
+    body: yup
+      .string(200, "The content has to be of 200 characters or longer")
+      .required("Here must be your article")
+  }),
   handleSubmit({ title, body, author, id, timestamp, category }, { props }) {
     submitPost({
       id,
